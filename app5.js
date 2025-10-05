cc17ea64ac6622140fdd11424ec51fdd1f909682
Tachobo <wilmerardila2001@gmail.com>
2025-10-04 19:22:06 -0500

    cuarto Commit

--- app5.js
+++ app5.js
@@ -2,7 +2,6 @@ alert(`En este programa se resolvera una operacion matematica misteriosa
 (20+10)/5+3∗2−4(20+10)/5+3*2-4(20+10)/5+3∗2−4`);
 let resultado = (20+10)/5+3*2-4*(20+10)/5+3*2-4*(20+10)/5+3*2-4;
 // resuelven primero los parentesis
-// luego las divisiones y multiplicaciones de izquierda a derecha
 resultado = 30/5+3*2-4*30/5+3*2-4*30/5+3*2-4;
 // luego las divisiones y multiplicaciones de izquierda a derecha
 resultado = 6+6-120/5+6-120/5+6-4
